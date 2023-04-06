0f005061de2b352df7d936d9b89daa3b6c637298
Arbi0930 <mbsavage0930@gmail.com>
2023-04-07 03:51:25 +0800

    lab 5 fixed

--- lab5/main1.py
+++ lab5/main1.py
@@ -1,11 +1,11 @@
 import pandas as pd 
 from urllib.request import urlopen
-
 from pgmpy.models import BayesianModel
 
-names = "A,B,C,D,E,F,G,H,I,J,K,L,M RESULT"
-names = names.split(",")
+names = "A,B,C,D,E,F,G,H,I,J,K,L,M,үр дүн"
+data = pd.read_csv(urlopen("https://bit.do/heart-disease"), names=names)
 
-data = pd.read_csv(urlopen("https://bit.do/heart-disease"),names=names)
-
-data.head()
+model = BayesianModel([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'E'), ('E', 'F'),
+                       ('F', 'G'), ('G', 'H'), ('H', 'I'), ('I', 'J'), ('J', 'RESULT'), 
+                       ('K', 'үр дүн'), ('L', 'үр дүн'), ('M', 'үр дүн')])
+model.fit(data)
